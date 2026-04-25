f591d6b82b019fc30bed722fecb211d343b34d48
SreekarDugyala <113871591+SreekarDugyala@users.noreply.github.com>
2026-04-24 21:34:17 -0500

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        //Sort
+        // char[] sSort = s.toCharArray();
+        // char[] tSort = t.toCharArray();
+
+        // Arrays.sort(sSort);
+        // Arrays.sort(tSort);
+
+        // return Arrays.equals(sSort, tSort);
+
+        // HashMap
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return countS.equals(countT);
+
+
+        // int [] refChars = new int[26];
+        // for(int i=0; i<s.length();i++){
+        //     refChars[s.charAt(i)-'a']++;
+        //     refChars[t.charAt(i)-'a']--;
+        // }
+        // for(int refChar : refChars){
+        //     if(refChar != 0) return false;
+        // }
+        // return true;
+    }
+}
